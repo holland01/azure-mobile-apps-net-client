8a3423314ac55441edaf39ec4f9734e76dbbb181
holland schutte <hgschutte1@gmail.com>
2017-05-26 17:53:28 -0700

    switch to netstandard1.3 only

--- src/Microsoft.Azure.Mobile.Client/Platform/Platform.cs
+++ src/Microsoft.Azure.Mobile.Client/Platform/Platform.cs
@@ -5,151 +5,12 @@
 using System;
 using System.Globalization;
 using System.Reflection;
-
-#if NETSTANDARD1_4
 using System.Runtime.InteropServices;
-#endif 
+using PCLStorage;
+
 
 namespace Microsoft.WindowsAzure.MobileServices
 {
-
-    /// <summary>
-    /// Set any callbacks and properties here to your
-    /// heart's content if you're using NetStandard 1.4; otherwise,
-    /// this is useless. Make sure they're
-    /// set before Platform.Instance is queried.
-    /// </summary>
-    public static class PlatformDetail
-    {
-#if NETSTANDARD1_4
-        public interface IApplicationStorageOps
-        {
-            System.IO.FileStream GetFileStream(string path, System.IO.FileMode mode, System.IO.FileAccess access);
-        }
-
-        public static IApplicationStorageOps AppStorageOps { get; set; }
-
-        /// <summary>
-        /// Path to the user's local application data storage. E.g., on windows this is often %USERPROFILE%\AppData\Local\YOUR_APPLICATION_NAME_HERE
-        /// </summary>
-        public static string LocalApplicationDataDirectory { get; set; }
-
-        /// <summary>
-        /// The version string of the operating system, apparently for an HTTP user-agent string.
-        /// </summary>
-        public static string OSVersion { get; set; }
-
-        /// <summary>
-        /// Windows net45 runtime reports this as "Win32NT"; apparently this is for an HTTP user-agent string.
-        /// That's all I can really provide.
-        /// </summary>
-        public static string OSPlatformArch { get; set; }
-
-        /// <summary>
-        /// The name of the operating system.
-        /// </summary>
-        public static string OSName { get; set; }
-
-        /// <summary>
-        /// Matched OSPlatform result according to RuntimeInformation.IsOSPlatform(),
-        /// or a thrown exception if no match is found.
-        /// </summary>
-        public static OSPlatform PlatformOrdinal
-        {
-            get
-            {
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                {
-                    return OSPlatform.Windows;
-                }
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                {
-                    return OSPlatform.OSX;
-                }
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                {
-                    return OSPlatform.Linux;
-                }
-
-                throw new InvalidOperationException("Could not identify operating system; RuntimeInformation.IsOSPlatform() returned false for all 3 values");
-            }
-        }
-
-        internal static void Validate()
-        {
-            if (string.IsNullOrEmpty(LocalApplicationDataDirectory))
-            {
-                throw new NullReferenceException("PlatformDetail.LocalApplicationDataDirectory must be set");
-            }
-
-            if (AppStorageOps == null)
-            {
-                throw new NullReferenceException("AppStorageOps cannot be null.");
-            }
-
-            string defaultName = PlatformOrdinal.ToString();
-
-            if (defaultName == "OSX")
-            {
-                defaultName = "MacOS";
-            }
-
-            string[] desc = RuntimeInformation.OSDescription.Split(' ');
-
-            
-            if (string.IsNullOrEmpty(OSVersion))
-            {
-                OSVersion = desc[2];
-            }
-
-            if (string.IsNullOrEmpty(OSPlatformArch))
-            {
-                OSPlatformArch = defaultName;
-            }
-
-            if (string.IsNullOrEmpty(OSName))
-            {
-                OSName = defaultName;
-            }
-        }
-#else
-         /// <summary>
-        /// Path to the user's local application data storage. E.g., on windows this is often %USERPROFILE%\AppData\Local\YOUR_APPLICATION_NAME_HERE
-        /// </summary>
-        public static string LocalApplicationDataDirectory { get { throw new InvalidOperationException("This class is only meant to be used with .NET Core"); } }
-
-        /// <summary>
-        /// The version string of the operating system, apparently for an HTTP user-agent string.
-        /// </summary>
-        public static string OSVersion { get { throw new InvalidOperationException("This class is only meant to be used with .NET Core"); } }
-
-        /// <summary>
-        /// Windows net45 runtime reports this as "Win32NT"; apparently this is for an HTTP user-agent string.
-        /// That's all I can really provide.
-        /// </summary>
-        public static string OSPlatformArch { get { throw new InvalidOperationException("This class is only meant to be used with .NET Core"); } }
-
-        /// <summary>
-        /// The name of the operating system.
-        /// </summary>
-        public static string OSName { get { throw new InvalidOperationException("This class is only meant to be used with .NET Core"); } }
-
-        /// <summary>
-        /// Dummy stub.
-        /// </summary>
-        public enum OSPlatform {}
-
-        /// <summary>
-        /// Matched OSPlatform result according to RuntimeInformation.IsOSPlatform(),
-        /// or a thrown exception if no match is found.
-        /// </summary>
-        public static OSPlatform PlatformOrdinal
-        {
-            get { throw new InvalidOperationException("This class is only meant to be used with .NET Core"); }
-        }
-#endif
-    }
-
 
     /// <summary>
     /// Provides access to platform-specific framework API's.
@@ -175,15 +36,7 @@ namespace Microsoft.WindowsAzure.MobileServices
                 // create if not yet created
                 if (current == null)
                 {
-#if NETSTANDARD1_4
-                    PlatformDetail.Validate();
-#endif
-
-#if !NETSTANDARD1_1
                     current = new CurrentPlatform();
-#else
-                    throw new PlatformNotSupportedException("TODO...");
-#endif
                 }
 
                 return current;
---
+++ src/Microsoft.Azure.Mobile.Client/Platforms/netstandard1_3/PlatformInformation.cs
@@ -0,0 +1,156 @@
+﻿// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+using System;
+using System.Globalization;
+using System.Reflection;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.WindowsAzure.MobileServices
+{
+    /// <summary>
+    /// Implements the <see cref="IPlatform"/> interface for the Windows
+    /// Phone platform.
+    /// </summary>
+    internal class PlatformInformation : IPlatformInformation
+    {
+        /// <summary>
+        /// The version string of the operating system, apparently for an HTTP user-agent string.
+        /// </summary>
+        public static string OSVersion { get; private set; }
+
+        /// <summary>
+        /// Windows net45 runtime reports this as "Win32NT"; apparently this is for an HTTP user-agent string.
+        /// That's all I can really provide.
+        /// </summary>
+        public static string OSPlatformArch { get; private set; }
+
+        /// <summary>
+        /// The name of the operating system.
+        /// </summary>
+        public static string OSName { get; private set; }
+
+        /// <summary>
+        /// A singleton instance of the <see cref="ApplicationStorage"/>.
+        /// </summary>
+        private static IPlatformInformation instance = new PlatformInformation();
+
+        /// <summary>
+        /// A singleton instance of the <see cref="ApplicationStorage"/>.
+        /// </summary>
+        public static IPlatformInformation Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Matched OSPlatform result according to RuntimeInformation.IsOSPlatform(),
+        /// or a thrown exception if no match is found.
+        /// </summary>
+        public static OSPlatform PlatformOrdinal
+        {
+            get
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    return OSPlatform.Windows;
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    return OSPlatform.OSX;
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    return OSPlatform.Linux;
+                }
+
+                throw new InvalidOperationException("Could not identify operating system; RuntimeInformation.IsOSPlatform() returned false for all 3 values");
+            }
+        }
+
+        static PlatformInformation()
+        {
+            string defaultName = PlatformOrdinal.ToString();
+
+            if (defaultName == "OSX")
+            {
+                defaultName = "MacOS";
+            }
+
+            string[] desc = RuntimeInformation.OSDescription.Split(' ');
+
+            OSVersion = desc[2];
+            OSPlatformArch = defaultName;
+            OSName = defaultName;
+        }
+
+        /// <summary>
+        /// The architecture of the platform.
+        /// </summary>
+        public string OperatingSystemArchitecture
+        {
+            get
+            {
+                return OSPlatformArch;
+            }
+        }
+
+        /// <summary>
+        /// The name of the operating system of the platform.
+        /// </summary>
+        public string OperatingSystemName
+        {
+            get
+            {
+                return OSName;
+            }
+        }
+
+        /// <summary>
+        /// The version of the operating system of the platform.
+        /// </summary>
+        public string OperatingSystemVersion
+        {
+            get
+            {
+                /*
+                Version version = Environment.OSVersion.Version;
+
+                return string.Format(CultureInfo.InvariantCulture,
+                    "{0}.{1}.{2}.{3}",
+                    version.Major,
+                    version.Minor,
+                    version.Revision,
+                    version.Build
+                );
+                */
+
+                return OSVersion;
+            }
+        }
+
+        /// <summary>
+        /// Indicated whether the device is an emulator or not
+        /// </summary>
+        public bool IsEmulator
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public string Version
+        {
+            get
+            {
+                return this.GetVersionFromAssemblyFileVersion();
+            }
+        }
+    }
+}
